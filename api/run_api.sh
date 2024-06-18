f3bfeee8a1cc05404a2bd6d2b8b1d9112dfc6fd8
zhihengw <zhihengw@gmail.com>
2024-06-19 01:48:32 +0800

    Prepare the run api/web locally.

--- api/run_api.sh
+++ api/run_api.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source ../venv/bin/activate
-# pip install -r requirements.txt
-# flask db upgrade
+pip install -r requirements.txt
+flask db upgrade
 
 ps -ef | grep celery |grep -v grep |awk '{print $2}' | xargs kill
 nohup celery -A app.celery worker -P gevent -c 1 -Q dataset,generation,mail --loglevel INFO 2>&1 >> logs/celery.log &
