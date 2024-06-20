c2022619348e8d71ba324605116dcee5af629294
qany-qtest <zhihengw@gmail.com>
2024-06-20 21:39:57 +0800

    Allow app to run locally.

--- api/run_api.sh
+++ api/run_api.sh
@@ -8,7 +8,7 @@ ps -ef | grep celery |grep -v grep |awk '{print $2}' | xargs kill
 nohup celery -A app.celery worker -P gevent -c 1 -Q dataset,generation,mail --loglevel INFO 2>&1 >> logs/celery.log &
 
 ps -ef |grep 5001 | grep -v grep |awk '{print $2}' | xargs kill
-#nohup flask run --host 0.0.0.0 --port=5001 --debug 2>&1 >> logs/api.log &
-flask run --host 0.0.0.0 --port=5001 --debug
+nohup flask run --host 0.0.0.0 --port=5001 --debug 2>&1 >> logs/api.log &
+#flask run --host 0.0.0.0 --port=5001 --debug
 
 
