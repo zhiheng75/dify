1257111a37282eb15a98352b3549957241eb4db5
yueyaobiao <1342298335@qq.com>
2024-07-01 12:29:46 +0800

    logo and start shell

--- web/start.sh
+++ web/start.sh
@@ -1,5 +1,5 @@
 #npm install
 #npm run build
 port=24188
-ps -ef | grep $port |grep -v grep |awk '{print $2}'  | xargs -i kill {}
+ps -ef | grep $port |grep -v grep |awk '{print $2}'  | xargs -i kill  -9 {}
 nohup npm run start --port=$port --host=0.0.0.0    > web.log 2>&1 &
