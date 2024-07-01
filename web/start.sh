2303e5e49032aa759108e2d8fabb436f09e1121e
yueyaobiao <1342298335@qq.com>
2024-07-01 12:09:09 +0800

    deply for model turbo

---
+++ web/start.sh
@@ -0,0 +1,5 @@
+#npm install
+#npm run build
+port=24188
+ps -ef | grep $port |grep -v grep |awk '{print $2}'  | xargs -i kill {}
+nohup npm run start --port=$port --host=0.0.0.0    > web.log 2>&1 &
