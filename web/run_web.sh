483162a361a37661c8d18f39dcd35a979df21bfb
zhiheng75 <zhihengw@gmail.com>
2024-07-22 22:11:28 +0800

    fixing run scripts.

--- web/run_web.sh
+++ web/run_web.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 npm run build
-#npm run start --port=3000 2 >&1 >> logs/web.log & 
+#npm run start --port=3000 2 >&1 >> logs/web.log &
+mkdir -p logs
 nohup npm run start --port=3000 2 >&1 >> logs/web.log & 
