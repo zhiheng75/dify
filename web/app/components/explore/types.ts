1ee2cedf4fdd33ca82bda943ff5bf970505a9fd1
jinwei <dchjinwei@126.com>
2024-08-01 08:33:18 +0800

    为三峡使用qtest_user1@qrgraph.com和qtest_user2@qrgraph.com两个测试用

--- web/app/components/explore/types.ts
+++ web/app/components/explore/types.ts
@@ -1,2 +1,2 @@
-export const installedChatUserList = ['dchjinwei@126.com'] as string[]
-export const installedChatUserAppIdList = ['9d3b6a3a-8a8e-4394-b363-eca84bd007d1'] as string[]
+export const installedChatUserList = ['dchjinwei@126.com','qtest_user1@qrgraph.com','qtest_user2@qrgraph.com'] as string[]
+export const installedChatUserAppIdList = ['9d3b6a3a-8a8e-4394-b363-eca84bd007d1','31a82a73-1066-40ad-bce8-baca9b65f004','31a82a73-1066-40ad-bce8-baca9b65f004'] as string[]
